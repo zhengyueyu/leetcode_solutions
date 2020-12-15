a7b47a1c6aa33f21303578f99cd1d7d1f62b05c5
zhengyueyu <zhengyueyu@moyi365.com>
2020-12-15 20:38:32 +0800

    update

--- 剑指 Offer 25. 合并两个排序的链表.cpp
+++ 剑指 Offer 25. 合并两个排序的链表.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		ListNode* head = merge(l1, l2);
+		return head;
+	}
 
+	ListNode* merge(ListNode* l1, ListNode* l2) {
+		ListNode* minnode = nullptr;
+		if (l1 == nullptr) return l2;
+		if (l2 == nullptr) return l1;
+		if (l1->val <= l2->val) {
+			minnode = l1;
+			ListNode* tmp = merge(l1->next, l2);
+			minnode->next = tmp;
+		}
+		else if (l1->val > l2->val) {
+			minnode = l2;
+			ListNode* tmp = merge(l1, l2->next);
+			minnode->next = tmp;
+		}
+		return minnode;
+	}
+};
