6436e53da34e908aa3a837567e524bc8b232dfb0
zhengyueyu <zhengyueyu@moyi365.com>
2020-12-16 22:41:30 +0800

    update

---
+++ 剑指 Offer 27. 二叉树的镜像 (2).cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if(root == nullptr) return root;
+        swap(root->left, root->right);
+        if(root->left) mirrorTree(root->left);
+        if(root->right) mirrorTree(root->right);
+        return root;
+    }
+};
